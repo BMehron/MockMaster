2e4c7b94d04d21183f889a700050a8b96ea92d72
Daniil <49778314+DKarz@users.noreply.github.com>
2024-04-20 12:17:49 +0200

    add voice mes

---
+++ dans/voice_messages.py
@@ -0,0 +1,71 @@
+import streamlit as st
+from audiorecorder import audiorecorder
+from streamlit_extras.stylable_container import stylable_container
+from model import get_answer
+
+# App title
+st.set_page_config(page_title="🤗💬 HugChat")
+
+# Hugging Face Credentials
+with st.sidebar:
+    
+    st.title('🤗💬 HugChat')
+    # if ('EMAIL' in st.secrets) and ('PASS' in st.secrets):
+    #     st.success('HuggingFace Login credentials already provided!', icon='✅')
+    #     hf_email = st.secrets['EMAIL']
+    #     hf_pass = st.secrets['PASS']
+    # else:
+    #     hf_email = st.text_input('Enter E-mail:', type='password')
+    #     hf_pass = st.text_input('Enter password:', type='password')
+    #     if not (hf_email and hf_pass):
+    #         st.warning('Please enter your credentials!', icon='⚠️')
+    #     else:
+    #         st.success('Proceed to entering your prompt message!', icon='👉')
+    st.success('Proceed to entering your prompt message!', icon='👉')
+    # st.markdown('📖 Learn how to build this app in this [blog](https://blog.streamlit.io/how-to-build-an-llm-powered-chatbot-with-streamlit/)!')
+    
+# Store LLM generated responses
+if "messages" not in st.session_state.keys():
+    st.session_state.messages = [{"role": "assistant", "content": "How may I help you?"}]
+
+# Display chat messages
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+#st.title("Audio Recorder")
+with stylable_container(
+        key="bottom_content",
+        css_styles="""
+            {
+                position: fixed;
+                bottom: 120px;
+                right: 20px;
+            }
+            """,
+    ):
+        audio = audiorecorder("🎙️ start", "🎙️ stop")
+if len(audio) > 0:
+    st.audio(audio.export().read())  
+    audio.export("audio.mp3", format="mp3")
+    # extract text from audio.
+    prompt = " ".join(audio.recognize("en-US").split()[1:])
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.write(prompt)
+    st.write(f"Frame rate: {audio.frame_rate}, Frame width: {prompt}, Duration: {audio.duration_seconds} seconds")
+elif prompt := st.chat_input(disabled=False):
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user"):
+            st.write(prompt)
+
+        # Generate a new response if last message is not from assistant
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            # response = generate_response(prompt, hf_email, hf_pass) 
+            response = get_answer(prompt)
+            st.write(response) 
+    message = {"role": "assistant", "content": response}
+    st.session_state.messages.append(message)
+    
